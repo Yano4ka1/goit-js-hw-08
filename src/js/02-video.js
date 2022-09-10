32ffcada54c75d9bc43a4b44263f5b21413c93b6
Yana Kirdyai <kirdyai@ukr.net>
2022-09-10 21:20:03 +0300

    finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+setVideoTime();
+
+function onPlay(event) {
+    localStorage.setItem(STORAGE_KEY, event.seconds);
+}
+
+function setVideoTime() {
+    const videoPlayerCurrentTime = localStorage.getItem(STORAGE_KEY);
+
+    if (videoPlayerCurrentTime) {
+        player.setCurrentTime(videoPlayerCurrentTime).then(function (seconds) {
+        // seconds = the actual time that the player seeked to
+            }).catch(function(error) {
+                switch (error.name) {
+                    case 'RangeError':
+                        // the time was less than 0 or greater than the video’s duration
+                        break;
+                default:
+                    // some other error occurred
+                    break;
+                }
+            })
+    }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
 
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form')
+let formDataToSave = {};
+  
+formRef.addEventListener('input', throttle(onFormInput, 500))
+formRef.addEventListener('submit', onFormSubmit)
+
+fillInForm();
+
+function onFormInput(event) {
+  const previouslySavedFormData = localStorage.getItem(STORAGE_KEY);
+
+  if (previouslySavedFormData) {
+    const parsedFormData = JSON.parse(previouslySavedFormData);
+    formDataToSave = { ...parsedFormData };
+  }
+
+  formDataToSave[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formDataToSave));
+}
+
+function fillInForm() {
+  const savedFormData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedFormData) {
+    const parsedFormData = JSON.parse(savedFormData);
+
+    const keys = Object.keys(parsedFormData);
+    keys.forEach(key => formRef[key].value = parsedFormData[key]);
+  }
+}
+
+function onFormSubmit(event) {
+  const form = event.target;
+
+  event.preventDefault();
+  console.log('SubmitedFormData',outputFormData(form));
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function outputFormData(form) {
+  const submitedFormData = {};
+  
+  const formData = new FormData(form);
+  for (let key of formData.keys()) {
+    submitedFormData[key] = formData.get(key);
+  }
+  
+  return submitedFormData;
+}
