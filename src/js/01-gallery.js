d50db6712e0ad5dabee01b47525b759c0d095133
Yana Kirdyai <kirdyai@ukr.net>
2022-09-10 10:30:02 +0300

    simpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,31 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+
+const galleryRef = document.querySelector('.gallery');
+
+function createGalleryMarkup(galleryItems) {
+
+    const galleryMarkup = galleryItems
+    .map(({preview,original,description}) => 
+        `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    ).join('')
+
+    return galleryMarkup;
+}
+
+galleryRef.insertAdjacentHTML('beforeend',createGalleryMarkup(galleryItems))
+
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.options.captionsData = 'alt';
+gallery.options.captionDelay = 250;
