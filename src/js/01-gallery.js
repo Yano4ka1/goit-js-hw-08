a245135fc4f5de25f28cfb6c786d92b2cd5f8950
Yana Kirdyai <kirdyai@ukr.net>
2022-09-11 12:30:37 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,5 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
-// Описаний в документації
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
+import SimpleLightbox from './simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
